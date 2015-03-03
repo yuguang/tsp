c38bc194eddadf8f5a36acd64fad1ee52eb8cf96
Shawn Brunsting <sjbrunst@users.noreply.github.com>
2015-03-03 16:52:14 -0500

    Begin implementing BHK algorithm

--- bhk.cpp
+++ bhk.cpp
@@ -1,4 +1,6 @@
 #include "bhk.h"
+#include <vector>
+#include <limits>
 
 bhk::bhk() {
 
@@ -8,11 +10,33 @@ bhk::~bhk() {
 	
 }
 
-void bhk::init(int ncount, int ecount, int *elist) {
+int bhk::getDistance(int city1, int city2) {
+	std::pair<int,int> key = std::make_pair(city1,city2);
+	if (this->distance.count(key) > 0) {
+		return this->distance[std::make_pair(city1,city2)];
+	}
+	return std::numeric_limits<int>::max(); // if there is no edge, we return INT_MAX
+}
 
+void bhk::init(int ncount, int ecount, int *elist, int *elen) {
+	this->ncount = ncount;
+	this->ecount = ecount;
+	this->elist = elist;
+	this->elen = elen;
+	for (int i = 0; i < ecount; i++) {
+		this->distance[std::make_pair(elist[2*i],elist[2*i+1])];
+	}
 }
 
 double bhk::solve() {
+	std::map<std::vector<int>,int> DPmap; // stores dynamic programming results
+	for (int i = 1; i < this->ncount; i++) {
+		std::vector<int> mapKey;
+		mapKey.push_back(0);
+		mapKey.push_back(i);
+		DPmap[mapKey] = this->getDistance(0,i);
+	}
+	// TODO: rest of the algorithm
 	return 0.0;
 }
 
--- bhk.h
+++ bhk.h
@@ -1,17 +1,29 @@
 #ifndef bhk_H
 #define bhk_H
 
+#include <map>
+
 // This class implements the Bellman-Held-Karp algorithm
 class bhk {
 
 	private:
+		int ncount; // number of nodes
+		int ecount; // number of edges
+		int *elist; // list of edges
+		int *elen; // length of each edge
+
+		// returns the distance between two cities
+		int getDistance(int city1, int city2);
+
+		// stores the given distances from elen
+		std::map<std::pair<int,int>, int> distance;
 
 	public:
 
 		bhk();
 		~bhk();
 
-		void init(int ncount, int ecount, int *elist);
+		void init(int ncount, int ecount, int *elist, int *elen);
 
 		double solve();
 
