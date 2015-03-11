93b6a2a06e6d948430c21f78b7ab55d0d8b185dd
Shawn Brunsting <sjbrunst@users.noreply.github.com>
2015-03-11 15:13:00 -0400

    Add recursive BHK code from professor

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-SUBTOUROBJS= tour.o lp.o util.o graph.o tnode.o bhk.o
+SUBTOUROBJS= tour.o lp.o util.o graph.o tnode.o bhk.o bhk2.o
 CPLEX_LIB = /software/cplex/distribution/cplex/lib/x86-64_linux/static_pic/libcplex.a
 CPLEX_INCLUDE = /software/cplex/distribution/cplex/include/ilcplex
 
@@ -9,8 +9,8 @@ CPPFLAGS=  -O3 -g -Wall -pedantic -Wno-long-long -I$(CPLEX_INCLUDE)
 tour: $(SUBTOUROBJS)
 	  $(CC) $(CPPFLAGS) -o tour $(SUBTOUROBJS) $(CPLEX_LIB) -lm -lpthread
 
-test_tour: test_tour.o bhk.o
-	  $(CC) $(CPPFLAGS) -o test_tour test_tour.o bhk.o -lm -lpthread
+test_tour: test_tour.o bhk.o bhk2.o
+	  $(CC) $(CPPFLAGS) -o test_tour test_tour.o bhk.o bhk2.o -lm -lpthread
 
 test_graph: test_graph.o graph.o tnode.o
 	  $(CC) $(CPPFLAGS) -o test_graph test_graph.o graph.o tnode.o -lm -lpthread
@@ -25,5 +25,6 @@ graph.o: graph.cpp graph.h tnode.h
 tnode.o: tnode.cpp tnode.h
 test_graph.o: test_graph.cpp graph.h tnode.h
 bhk.o: bhk.cpp bhk.h
-test_tour.o: test_tour.cpp bhk.h
+bhk2.o: bhk2.cpp bhk2.h
+test_tour.o: test_tour.cpp bhk.h bhk2.h
 
--- test_tour.cpp
+++ test_tour.cpp
@@ -1,6 +1,7 @@
 #include <cstdio>
 #include <vector>
 #include "bhk.h"
+#include "bhk2.h"
 
 void checkValue(const char *method, int expectedValue, int testValue) {
 	printf("  %s: ", method);
@@ -17,43 +18,55 @@ int main (int ac, char **av) {
 
 	// Test 1: graph with 2 nodes
 	bhk* bhk_solver1 = new bhk();
+	bhk2* bhk2_solver1 = new bhk2();
 	int ncount1 = 2;
 	int ecount1 = 1;
 	int elist1[] = {0,1};
 	int elen1[] = {3};
 	bhk_solver1->init(ncount1, ecount1, elist1, elen1);
+	bhk2_solver1->init(ncount1, ecount1, elist1, elen1);
 	printf("Test 1:\n");
 	checkValue("BHK", 6, bhk_solver1->solve());
+	checkValue("BHK2", 6, bhk2_solver1->solve());
 	
 	// Test 2: graph with 3 nodes
 	bhk* bhk_solver2 = new bhk();
+	bhk2* bhk2_solver2 = new bhk2();
 	int ncount2 = 3;
 	int ecount2 = 3;
 	int elist2[] = {0,1, 0,2, 1,2};
 	int elen2[] = {3, 4, 5};
 	bhk_solver2->init(ncount2, ecount2, elist2, elen2);
+	bhk2_solver2->init(ncount2, ecount2, elist2, elen2);
 	printf("Test 2:\n");
 	checkValue("BHK", 12, bhk_solver2->solve());
+	checkValue("BHK2", 12, bhk2_solver2->solve());
 
 	// Test 3: graph with 4 nodes
 	bhk* bhk_solver3 = new bhk();
+	bhk2* bhk2_solver3 = new bhk2();
 	int ncount3 = 4;
 	int ecount3 = 6;
 	int elist3[] = {0,1, 1,2, 2,3, 0,3, 0,2, 1,3};
 	int elen3[] = {1,1,1,1,2,2};
 	bhk_solver3->init(ncount3, ecount3, elist3, elen3);
+	bhk2_solver3->init(ncount3, ecount3, elist3, elen3);
 	printf("Test 3:\n");
 	checkValue("BHK", 4, bhk_solver3->solve());
+	checkValue("BHK2", 4, bhk2_solver3->solve());
 	
 	// Test 4: graph with 6 nodes
 	bhk* bhk_solver4 = new bhk();
+	bhk2* bhk2_solver4 = new bhk2();
 	int ncount4 = 6;
 	int ecount4 = 15;
 	int elist4[] = {0,2, 2,5, 5,1, 1,3, 3,4, 4,0, 0,1, 0,3, 0,5, 1,2, 1,4, 2,3, 2,4, 3,5, 4,5};
 	int elen4[] = {2,3,5,4,7,6, 9,8,9,8,9,8,9,8,9};
 	bhk_solver4->init(ncount4, ecount4, elist4, elen4);
+	bhk2_solver4->init(ncount4, ecount4, elist4, elen4);
 	printf("Test 4:\n");
 	checkValue("BHK", 27, bhk_solver4->solve());
+	checkValue("BHK2", 27, bhk2_solver4->solve());
 
 	printf("Done.\n");
 	return 0;
