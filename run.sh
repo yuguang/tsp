3d1bbcbc8227bd5c26b6fdfa7f47a2a075ff4e6f
Yuguang Zhang <zh.yuguang.cn@gmail.com>
2015-03-19 12:46:08 -0400

    terminate after 5 minutes and try all 6 methods

--- run.sh
+++ run.sh
@@ -3,9 +3,9 @@ for i in `seq 50 1000`;
 do
   for j in `seq 5 25`;
   do
-    for k in `seq 1 5`;
+    for k in `seq 1 6`;
     do
-      timeout 900 ./tour -b $i -k $j -m $k
+      timeout 300 ./tour -b $i -k $j -m $k
     done
   done
 done   
