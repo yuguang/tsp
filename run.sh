22ccd8a04736a684026dc7070a6bba1b053be545
Yuguang Zhang <zh.yuguang.cn@gmail.com>
2015-03-19 13:29:13 -0400

    run through grid sizes in steps of 50

--- run.sh
+++ run.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
-for i in `seq 50 1000`;
+for i in `seq 1 200`;
 do
   for j in `seq 5 25`;
   do
     for k in `seq 1 6`;
     do
-      timeout 300 ./tour -b $i -k $j -m $k
+      timeout 300 ./tour -b $(($i*50)) -k $j -m $k
     done
   done
 done   
