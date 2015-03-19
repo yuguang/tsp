f82b38565f8b359772599a869ceb045983bcd393
Yuguang Zhang <zh.yuguang.cn@gmail.com>
2015-03-19 13:57:59 -0400

    go through only 3 grid sizes

--- run.sh
+++ run.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
-for i in `seq 1 200`;
+for i in 100, 1000, 5000;
 do
   for j in `seq 5 25`;
   do
     for k in `seq 1 6`;
     do
-      timeout 300 ./tour -b $(($i*50)) -k $j -m $k
+      timeout 300 ./tour -b $i -k $j -m $k
     done
   done
 done   
